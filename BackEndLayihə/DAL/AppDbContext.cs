00e4b138b362a616f9bfa15598ae20cd917d8b8d
Vusalmb <53233126+Vusalmb@users.noreply.github.com>
2022-01-31 21:30:25 +0400

    update

--- BackEndLayihə/DAL/AppDbContext.cs
+++ BackEndLayihə/DAL/AppDbContext.cs
@@ -16,5 +16,16 @@ namespace BackEndLayihə.DAL
         public DbSet<Setting> Settings { get; set; }
         public DbSet<Slider> Sliders { get; set; }
         public DbSet<WelcomeEduHome> WelcomeEduHomes { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Hobby> Hobbies { get; set; }
+        public DbSet<Faculity> Faculities { get; set; }
+        public DbSet<TeacherHobby> TeacherHobbies { get; set; }
+        public DbSet<TeacherFaculity> TeacherFaculities { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<CourseFeature> CourseFeatures { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CourseCategory> CourseCategories { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<CourseTag> CourseTags { get; set; }
     }
 }
