0c4b0405c8ae4a2ba1ecd63b1aadc388d9ad2777
Vusalmb <53233126+Vusalmb@users.noreply.github.com>
2022-01-31 21:29:42 +0400

    create course and teacher controllers

---
+++ BackEndLayihə/Controllers/CourseController.cs
@@ -0,0 +1,41 @@
+﻿using BackEndLayihə.DAL;
+using BackEndLayihə.Models;
+using BackEndLayihə.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEndLayihə.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CourseController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            CourseVM model = new CourseVM
+            {
+                Courses = _context.Courses.ToList()
+            };
+            return View(model);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            Course course = _context.Courses.FirstOrDefault(c => c.Id == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+            return View(course);
+        }
+    }
+}
---
+++ BackEndLayihə/Controllers/TeacherController.cs
@@ -0,0 +1,41 @@
+﻿using BackEndLayihə.DAL;
+using BackEndLayihə.Models;
+using BackEndLayihə.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEndLayihə.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TeacherController(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+        public IActionResult Index()
+        {
+            TeacherVM model = new TeacherVM
+            {
+                Teachers = _context.Teachers.ToList()
+            };
+            return View(model);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            Teacher teacher = _context.Teachers.Include(t=>t.TeacherHobbies).ThenInclude(th=>th.Hobby).Include(t=>t.TeacherFaculities).ThenInclude(tf=>tf.Faculity).FirstOrDefault(t => t.Id == id);
+            if(teacher == null)
+            {
+                return NotFound();
+            }
+            return View(teacher);
+        }
+    }
+}
